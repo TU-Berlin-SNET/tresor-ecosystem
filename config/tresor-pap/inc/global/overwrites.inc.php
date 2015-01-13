83e3452e44cdc7bfcbf51e33f461fd32ec27b7a2
Mathias Slawik <mathias.slawik@tu-berlin.de>
2015-01-13 17:58:40 +0100

    Added TRESOR PAP

---
+++ config/tresor-pap/inc/global/overwrites.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+  /**
+   * Please include additional configuration here
+   */
+   define("BASE_URL", "http://pap.service.cloud-tresor.com/");
+   //define("LOGSTASH_URL", "logstash:9400");
+   define("BROKER_URL", "http://broker:3000");
+   define("PDP_URL", "http://pdp:8080");
